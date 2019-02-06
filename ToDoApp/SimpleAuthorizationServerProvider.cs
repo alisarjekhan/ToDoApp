6d2c99377d452bc771b8ef7ac8674c2233e4cfbc
alisarjekhan <msarjekhan@gmail.com>
2019-02-06 10:03:19 -0800

    Securing api layer using OAuth 2.0

---
+++ ToDoApp/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            //using (AuthRepository _repo = new AuthRepository())
+            //{
+            //    IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+            //    if (user == null)
+            //    {
+            //        context.SetError("invalid_grant", "The user name or password is incorrect.");
+            //        return;
+            //    }
+            //}
+
+            var _repo = new UserRepository();
+           
+            var user = _repo.FindUser(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+           
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType, ClaimTypes.Name, ClaimTypes.Role);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+            identity.AddClaim(new Claim(ClaimTypes.Role, user.Role));
+
+            context.Validated(identity);
+
+        }
+    }
+}
